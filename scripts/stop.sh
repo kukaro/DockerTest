5b578ac80c92c39af52e016189c713067586e841
coeat.k <coeat.k@kakaocorp.com>
2024-04-15 19:18:37 +0900

    feat:순서 변경

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,6 +3,6 @@
 echo "#Stopping the container..."
 #service docker start
 if [ "$(docker ps -q -a --filter ancestor=groom-test-docker | wc -l)" -gt 0 ]; then
-  docker rm groom-test-docker
   docker stop groom-test-docker
+  docker rm groom-test-docker
 fi
