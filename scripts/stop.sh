2c67852d742cb137c1209eec0cc808f3b658cf4b
coeat.k <coeat.k@kakaocorp.com>
2024-04-15 19:26:21 +0900

    feat:필터 구문 제외

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,7 +2,7 @@
 
 echo "#Stopping the container..."
 #service docker start
-if [ "$(docker ps -q -a --filter ancestor=groom-test-docker | wc -l)" -gt 0 ]; then
+if [ "$(docker ps -q -a | wc -l)" -gt 0 ]; then
   docker stop groom-test-docker
   docker rm groom-test-docker
 fi
