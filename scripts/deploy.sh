4f2b9b91ce62efc5eb97c06bf23bb64fefb9da3f
coeat.k <coeat.k@kakaocorp.com>
2024-04-15 17:21:00 +0900

    feat:스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,5 @@
 echo "#Starting the container..."
 docker load < groom-test-docker.tar
 tar -xvf groom-test-docker.tar
+docker images
 docker run -d -p 80:80 --name groom-test-docker groom-test-docker
