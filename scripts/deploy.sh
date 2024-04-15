f98716b66ef3d09cd2fd0ca36ae7e83a543aeacd
coeat.k <coeat.k@kakaocorp.com>
2024-04-15 17:06:57 +0900

    feat:service docker start

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo su
 docker load < groom-test-docker.tar
 tar -xvf groom-test-docker.tar
 docker run -d -p 80:80 --name groom-test-docker groom-test-docker
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo su
 echo "#Stopping the container..."
 service docker start
 docker stop groom-test-docker
